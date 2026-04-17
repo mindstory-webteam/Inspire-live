48ebd35ea4c835af540af2e2cf614a13db8eb4ce
jishnu-NKS-Tec <jishnunarayanan378@gmail.com>
2026-04-17 16:25:54 +0530

    changes

--- client/src/components/shared/buttons/ButtonPrimary.js
+++ client/src/components/shared/buttons/ButtonPrimary.js
@@ -1,33 +1,50 @@
+
 import Link from "next/link";
 
-const ButtonPrimary = ({ className, text, isTextBtn, url, type, iconName }) => {
+const ButtonPrimary = ({ className, text, isTextBtn, url, type, iconName, hideIcon }) => {
 	return (
 		<>
+			
+			<style>{`
+    .tj-primary-btn.no-icon.learning-btn {
+       padding: 16px !important;
+    }
+    .tj-primary-btn.no-icon.learning-btn .btn-text {
+        padding-right: 0 !important;
+        margin-right: 0 !important;
+    }
+    .tj-primary-btn.no-icon.learning-btn .btn-text span {
+        margin-right: 0 !important;
+    }
+`}</style>
+
 			{type ? (
 				<button
 					type={type ? type : "submit"}
-					className={`tj-primary-btn ${className ? className : ""}`}
+					className={`tj-primary-btn ${hideIcon ? "no-icon" : ""} ${className ? className : ""}`}
 				>
 					<span className="btn-text">
 						<span>{text}</span>
 					</span>
-					<span className="btn-icon">
-						<i className={iconName ? iconName : "tji-arrow-right-long"}></i>
-					</span>
+					{!hideIcon && (
+						<span className="btn-icon">
+							<i className={iconName ? iconName : "tji-arrow-right-long"}></i>
+						</span>
+					)}
 				</button>
 			) : (
 				<Link
 					href={url ? url : "/"}
-					className={`${isTextBtn ? "text-btn" : "tj-primary-btn"} ${
-						className ? className : ""
-					}`}
+					className={`${isTextBtn ? "text-btn" : "tj-primary-btn"} ${hideIcon ? "no-icon" : ""} ${className ? className : ""}`}
 				>
 					<span className="btn-text">
 						<span>{text}</span>
 					</span>
-					<span className="btn-icon">
-						<i className={iconName ? iconName : "tji-arrow-right-long"}></i>
-					</span>
+					{!hideIcon && (
+						<span className="btn-icon">
+							<i className={iconName ? iconName : "tji-arrow-right-long"}></i>
+						</span>
+					)}
 				</Link>
 			)}
 		</>
